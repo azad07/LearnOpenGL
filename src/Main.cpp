e6f762787905ab9c4940cbd82ef3550d96f140a2
Ashish Azad <ashishazad@Ashishs-MacBook-Pro.local>
2022-03-07 01:59:44 +0100

    Initial setup

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,64 @@
+#include <fstream>
 #include <iostream>
+#include <stdio.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
 
 using namespace std;
 
-int main() {
-    cout<<"ashish...."<<endl;
-    return 0;
-}
+const GLuint DEFAULT_WINDOW_WIDTH = 800, DEFAULT_WINDOW_HEIGHT = 800;
+const char* WINDOW_TITLE = "OpenGL Application";
+
+
+int main()
+{
+     GLFWwindow* window;
+
+    if (!glfwInit())
+        exit(1);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#endif
+
+    window = glfwCreateWindow(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT, WINDOW_TITLE, NULL, NULL);
+
+    if (!window)
+    {
+        glfwTerminate();
+        exit(1);
+    }
+
+    //glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+
+    if (glewInit() != GLEW_OK)
+        exit(1);
+
+
+
+    glClearColor(1.0f, 0.1f, 0.1f, 1.0f);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //shader->use();
+       // cube->draw();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    exit(0);
+}  
