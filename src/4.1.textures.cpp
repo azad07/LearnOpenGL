069da81415fe1b79c9d9b8e2e0e7ed477de91bb6
Ashish Azad <ashishazad@Ashishs-MacBook-Pro.local>
2022-03-27 13:50:26 +0200

    Texture

---
+++ src/4.1.textures.cpp
@@ -0,0 +1,316 @@
+#if 1
+
+#include <fstream>
+#include <iostream>
+#include <stdio.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+//#include "filesystem.h"
+// TODO: do we need this macro for stb_image ??
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+// TODO: check if glad is really needed??
+//#include <glad/glad.h>
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+using namespace std;
+
+// settings.
+const unsigned int DEFAULT_WINDOW_WIDTH = 800;
+const unsigned int DEFAULT_WINDOW_HEIGHT = 600;
+const char *WINDOW_TITLE = "Hellow Triangle";
+
+void framebuffer_size_callback(GLFWwindow *win, int width, int height);
+void processInput(GLFWwindow *win);
+unsigned int createShaderProgram(const char *vertexShaderSource, const char *fragmentShaderSource);
+
+const char *vertexShaderSource = "#version 330 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "layout (location = 1) in vec3 aColor;\n"
+                                 "layout (location = 2) in vec2 aTexCoord;\n"
+                                 "out vec3 ourColor;\n"
+                                 "out vec2 TexCoord;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "   gl_Position = vec4(aPos, 1.0);\n"
+                                 "   ourColor = aColor;\n"
+                                 "   TexCoord = vec2(aTexCoord.x, aTexCoord.y);\n"
+                                 "}\0";
+const char *fragmentShaderSource = "#version 330 core\n"
+                                   "out vec4 FragColor;\n"
+                                   "in vec3 ourColor;\n"
+                                   "in vec2 TexCoord;\n"
+                                   "uniform sampler2D texture1;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   "   FragColor = texture(texture1, TexCoord);\n"
+                                   "}\n\0";
+
+int main()
+{
+    // glfw: initialize and configure.
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#endif
+
+    // glfw window creation.
+    GLFWwindow *window = glfwCreateWindow(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT, WINDOW_TITLE, NULL, NULL);
+    if (window == nullptr)
+    {
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    glewExperimental = GL_TRUE;
+
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to init GLEW " << std::endl;
+        return -1;
+    }
+
+// TODO: check if glad is really needed on macos ??
+#if 0
+    // glad: load all opengl function pointers.
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+#endif
+
+    unsigned int shaderProgram = createShaderProgram(vertexShaderSource, fragmentShaderSource);
+
+    // set up vertex data (and buffers) and configure vertex attributes.
+    float vertices[] = {
+        // positions          // colors           // texture coords
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
+    };
+    unsigned int indices[] = {  
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+
+    unsigned int vbo; // vertex buffer object
+    unsigned int vao; // vertex array object
+    unsigned int ebo; // element buffer object
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
+
+    // bind the vertex array object first, then bind and set vertex buffers,
+    // and then configure vertex attributes.
+    glBindVertexArray(vao);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    unsigned int aColorAtrributeLocation = glGetAttribLocation(shaderProgram, "aColor");
+    glVertexAttribPointer(aColorAtrributeLocation, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(aColorAtrributeLocation);
+    // texture coord attribute
+    glVertexAttribPointer(3, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6*sizeof(float)));
+    glEnableVertexAttribArray(3);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+
+#if 0
+    // load and create a texture
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering paramentes
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load image, create texture and generate mipmaps
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("/Users/ashishazad/Desktop/LearnVulkan/openGL/LearnOpenGL/LearnOpenGL/resource/textures/container.jpg",
+     &width, &height, &nrChannels, 0);
+    
+    std::cout<<"Width: "<<width<<" Height: "<<height<<" nrChannels: "<< nrChannels<<std::endl;
+    if(data)
+    {
+        //glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout<<"ERROR:: Failed to load texture."<<std::endl;
+    }
+    std::cout<<"LINE: "<<__LINE__<<std::endl;
+    stbi_image_free(data);
+    std::cout<<"LINE: "<<__LINE__<<std::endl;
+#else
+
+unsigned int texid;
+glGenTextures(1, &texid);
+//glActiveTexture(GL_TEXTURE0);
+glBindTexture(GL_TEXTURE_2D, texid);
+glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+// Black/white checkerboard
+float pixels[] = {
+    0.0f, 0.0f, 0.0f,   1.0f, 1.0f, 1.0f,
+    1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f
+};
+glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 2, 2, 0, GL_RGB, GL_FLOAT, pixels);
+
+glGenerateMipmap(GL_TEXTURE_2D);
+
+#endif
+
+    // uncomment this call to draw in wireframe polygons.
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    // render loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        processInput(window);
+
+        // render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // bind texture
+        glBindTexture(GL_TEXTURE_2D, texid);
+        // draw a triangle
+        glUseProgram(shaderProgram);
+        glBindVertexArray(vao);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        // glfw: swap buffers and poll IO events(keys pressed/released, mouse moved etc.)
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    glDeleteBuffers(1, &ebo);
+
+    glfwTerminate();
+
+    return 0;
+}
+
+/**
+ * @brief Create a Shader Program object
+ *
+ * @param vertexShaderSource    vertex shader source
+ * @param fragmentShaderSource  fragment shader source.
+ * @return unsigned int         shader program id on success, -1 on fail.
+ */
+unsigned int createShaderProgram(const char *vertexShaderSource, const char *fragmentShaderSource)
+{
+    if (vertexShaderSource == nullptr || fragmentShaderSource == nullptr)
+    {
+        std::cout << "ERROR:: vertex/fragment shader source is nullptr" << std::endl;
+        return -1; // TODO: this is bad
+    }
+    // build and compile our shader program.
+    // vertex shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile error.
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (success == false)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FALIED\n"
+                  << infoLog << std::endl;
+    }
+
+    // fragment shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile error.
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (success == false)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FALIED\n"
+                  << infoLog << std::endl;
+    }
+
+    // link shaders
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (success == false)
+    {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+                  << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
+}
+
+/**
+ * @brief whenever the window size changed (by OS or user resize) this callback function executes.
+ *
+ * @param win   window handle
+ * @param width widnow width
+ * @param height window height
+ */
+void framebuffer_size_callback(GLFWwindow *win, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions.
+    glViewport(0, 0, width, height);
+}
+
+/**
+ * @brief process all input.
+ *
+ * @param win  window handle.
+ */
+void processInput(GLFWwindow *win)
+{
+    if (glfwGetKey(win, GLFW_KEY_ENTER) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(win, true);
+    }
+}
+
+#endif
